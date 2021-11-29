54c9923db01ae09554609865812faa6e1af670dc
rcaayush <48044923+rcaayush@users.noreply.github.com>
2021-11-29 12:21:45 +0530

    Create Test1.java
    name changed accordingly

--- TestProject/src/Classess/Test1.java
+++ TestProject/src/Classess/Test1.java
@@ -3,9 +3,9 @@ package Classess;
 public class Test1 {
 
 	public static void main(String[] args) {
-			int x=10,y=20;
+			int data1=10,data2=20;
 			System.out.println("Adding two numbers");
-			System.out.println(x+y);
+			System.out.println(data2+data1);
 			
 	}
 
